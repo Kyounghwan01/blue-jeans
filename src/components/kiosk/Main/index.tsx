1429792156c4d1899b2559ce2ab3a45e7672d7a4
Kyounghwan01 <noh5524@gmail.com>
2022-01-11 23:31:23 +0900

    feature: kiosk main 화면 퍼블

--- src/components/kiosk/Main/index.tsx
+++ src/components/kiosk/Main/index.tsx
@@ -1,3 +1,8 @@
+import styled from "styled-components";
+import RemoveSharpIcon from "@mui/icons-material/RemoveSharp";
+import AddSharpIcon from "@mui/icons-material/AddSharp";
+import { kioskProducts, kioskTab } from "utils/constants";
+
 const Index = ({
   next,
   back
@@ -6,12 +11,217 @@ const Index = ({
   back: () => Promise<boolean>;
 }) => {
   return (
-    <div>
-      두번째 intro
-      <button onClick={back}>이전</button>
-      <button onClick={next}>다음</button>
-    </div>
+    <Block>
+      <section className="tab">
+        {kioskTab.map(el => (
+          <div key={el.type}>
+            <span>{el.label}</span>
+          </div>
+        ))}
+      </section>
+      {/* <button onClick={back}>이전</button>
+      <button onClick={next}>다음</button> */}
+      <div className="main">
+        {kioskProducts
+          .filter(product => product.type === "fork")
+          .map(product => (
+            <div className="main__product-wrapper" key={product.name}>
+              <div className="main__product-wrapper__img"></div>
+              <div className="main__product-wrapper__content">
+                {product.name} <br /> {product.price}
+              </div>
+            </div>
+          ))}
+      </div>
+      <div className="bill">
+        <div className="bill__list">
+          <div className="bill__list__divide">
+            <div>메뉴</div>
+            <div>수량</div>
+            <div>가격</div>
+            <div></div>
+          </div>
+          <div className="bill__list__menu">
+            <div className="bill__list__menu__wrapper">
+              <div>01 무공돈까스</div>
+              <div className="bill__list__menu__count">
+                <div
+                  className="icon-wrap"
+                  style={{
+                    background: "dodgerblue",
+                    width: "25px",
+                    height: "25px",
+                    borderRadius: "4px",
+                    textAlign: "center",
+                    display: "inline-block"
+                  }}
+                >
+                  <RemoveSharpIcon style={{ color: "white" }} />
+                </div>
+
+                <div style={{ margin: "0 5px", display: "inline-block" }}>
+                  2
+                </div>
+                <div
+                  className="icon-wrap"
+                  style={{
+                    background: "dodgerblue",
+                    width: "25px",
+                    height: "25px",
+                    borderRadius: "4px",
+                    textAlign: "center",
+                    display: "inline-block"
+                  }}
+                >
+                  <AddSharpIcon style={{ color: "white" }} />
+                </div>
+              </div>
+              <div>17,8000</div>
+              <div>x</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bill__footer">
+          <div className="bill__footer__count">
+            <div className="bill__footer__count__amount">
+              <div>주문수량</div>
+              <div>2</div>
+            </div>
+            <div className="bill__footer__count__price">
+              <div>주문금액</div>
+              <div>17,800</div>
+            </div>
+          </div>
+          <div className="bill__footer__card">
+            <div className="bill__footer__card__cancel">
+              전체
+              <br />
+              취소
+            </div>
+            <div className="bill__footer__card__payment">payco</div>
+            <div className="bill__footer__card__payment">카드결제</div>
+          </div>
+        </div>
+      </div>
+    </Block>
   );
 };
+
+const Block = styled.article`
+  .tab {
+    padding: 10px 18px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    gap: 5px;
+    div {
+      text-align: center;
+      background: red;
+      height: 30px;
+    }
+  }
+  .main {
+    padding: 10px 9px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 5px;
+    &__product-wrapper {
+      border-radius: 4px;
+      background-color: grey;
+      &__img {
+        border-radius: 4px 4px 0 0;
+        background-color: #aaa;
+        height: 80px;
+      }
+      &__content {
+        color: white;
+        padding: 5px 0;
+        text-align: center;
+      }
+    }
+  }
+
+  .bill {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    &__list {
+      /* display: flex; */
+      &__divide {
+        background: grey;
+        display: grid;
+        grid-template-columns: 5fr 3fr 2fr 1fr;
+        padding: 0 18px;
+        color: white;
+        div {
+          &:not(:first-child) {
+            text-align: center;
+          }
+        }
+      }
+      &__menu {
+        padding: 10px 10px;
+        background: #eee;
+        height: 20vh;
+        border-bottom: 1px solid grey;
+        overflow-y: auto;
+        &__wrapper {
+          padding-bottom: 8px;
+          display: grid;
+          grid-template-columns: 5fr 3fr 2fr 1fr;
+        }
+        &__count {
+          display: flex;
+          justify-content: center;
+        }
+      }
+    }
+
+    &__footer {
+      display: flex;
+      justify-content: space-between;
+      padding: 6px 8px;
+      background: #eee;
+      &__count {
+        background: white;
+        padding: 5px 10px;
+        width: 100%;
+        margin-right: 20px;
+        border-radius: 8px;
+        &__amount {
+          display: flex;
+          justify-content: space-between;
+          color: #aaa;
+        }
+        &__price {
+          display: flex;
+          justify-content: space-between;
+          color: #aaa;
+        }
+      }
+      &__card {
+        display: flex;
+        &__cancel {
+          padding: 5px 8px;
+          background: grey;
+          color: white;
+          margin-right: 5px;
+          border-radius: 4px;
+          width: 40px;
+          text-align: center;
+        }
+        &__payment {
+          margin-right: 5px;
+          padding: 5px 8px;
+          background: red;
+          color: white;
+          border-radius: 4px;
+          width: 50px;
+          text-align: center;
+        }
+      }
+    }
+  }
+`;
 
 export default Index;
--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -40,3 +40,79 @@ export const QuestionType = [
     label: "기타"
   }
 ];
+
+export const kioskProducts = [
+  {
+    type: "fork",
+    name: "무공돈까스",
+    price: 8900
+  },
+  {
+    type: "fork",
+    name: "치즈돈까스",
+    price: 7900
+  },
+  {
+    type: "fork",
+    name: "옛날돈까스",
+    price: 7900
+  },
+  {
+    type: "fork",
+    name: "함박스테이크",
+    price: 8500
+  },
+  {
+    type: "rice",
+    name: "돈부리",
+    price: 8500
+  },
+  {
+    type: "rice",
+    name: "김치볶음밥",
+    price: 8500
+  },
+  {
+    type: "noodle",
+    name: "칼국수",
+    price: 8500
+  },
+  {
+    type: "noodle",
+    name: "쌀국수",
+    price: 18500
+  },
+  {
+    type: "side",
+    name: "밥",
+    price: 1000
+  },
+  {
+    type: "side",
+    name: "면",
+    price: 1000
+  },
+  {
+    type: "drink",
+    name: "콜라",
+    price: 2000
+  },
+  {
+    type: "drink",
+    name: "사이다",
+    price: 2000
+  },
+  {
+    type: "drink",
+    name: "환타",
+    price: 2000
+  }
+];
+
+export const kioskTab = [
+  { type: "fork", label: "돈까스류" },
+  { type: "rice", label: "덮밥류" },
+  { type: "noodle", label: "면류" },
+  { type: "side", label: "사이드류" },
+  { type: "drink", label: "음류/주류" }
+];
