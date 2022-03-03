6a8e343ebd2a7650dfb3532c844fc1b318af2b05
Joshua(노경환) <khnoh@carrotins.com>
2022-03-03 19:45:01 +0900

    private chat poc

--- src/pages/open-chat/index.tsx
+++ src/pages/open-chat/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import BasicLayout from "components/common/BasicLayout";
 import setDocFirebase from "utils/api/setDocFirebase";
-import { collection, query, getDocs } from "firebase/firestore";
+import { collection, query, getDocs, where } from "firebase/firestore";
 import { db } from "utils/api/firebase";
 import Link from "next/link";
 import AddIcon from "@mui/icons-material/Add";
@@ -25,42 +25,47 @@ const Index = () => {
   const getRoom = async () => {
     // 일단 다 긁어오기
     const roomRef = collection(db, "chat-room");
-    const data = await getDocs(await query(roomRef));
-    const roomList = data.docs.map(doc => {
+    const data = await getDocs(
+      await query(roomRef, where("member", "array-contains", "2042204892"))
+    );
+    const roomList = data.docs.map((doc) => {
       return { id: doc.id, ...doc.data() };
     }) as IChatRoom[];
+    console.log(roomList);
     setRoom(roomList);
   };
 
   const createOpenChat = async () => {
     const res = await setDocFirebase({
+      // chat-room || private-chat
       dbColumn: "chat-room",
       setType: "anonymous",
       // owner: user-uid
       payload: {
         owner: "2042204892",
-        title: 222,
+        title: "ban test",
         desc: 333,
         tag: [1, 2, 3],
-        memberCount: 1
-      }
+        isSecret: false,
+        member: [{ name: "2042204892", ban: [] }],
+      },
     });
 
-    const resCreateMember = await setDocFirebase({
-      dbColumn: "chat-room-member",
-      dbKey: String(res.id),
-      setType: "selectKey",
-      payload: {
-        owner: "2042204892",
-        member: ["2042204892"]
-      }
-    });
-    console.log(resCreateMember);
+    // const resCreateMember = await setDocFirebase({
+    //   dbColumn: "chat-room-member",
+    //   dbKey: String(res.id),
+    //   setType: "selectKey",
+    //   payload: {
+    //     owner: "2042204892",
+    //     member: ["2042204892"],
+    //   },
+    // });
+    // console.log(resCreateMember);
   };
 
-  // owner, title, desc, img, tag
+  // owner, title, desc, tag
 
-  // todo: list에 paging해야하는데 sort type이 뭔지 모르겠네
+  // todo: list에 paging해야하는데 sort type이 뭔지 모르겠네, delete하면 방도 나가고, 글도 없어지고
 
   return (
     <BasicLayout
@@ -69,13 +74,13 @@ const Index = () => {
       footer={true}
       loading={false}
     >
-      {room.map(el => (
+      {room.map((el) => (
         <Link key={el.id} href={`/open-chat/${el.id}`}>
           <a
             style={{
               border: "1px solid black",
               display: "block",
-              margin: "10px"
+              margin: "10px",
             }}
           >
             {el.title} <span>{el.memberCount}명</span> <span>{el.desc}</span>
